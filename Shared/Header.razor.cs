a9400d2aa8613687e8ff1b09e5504f3add7fcb9a
Tien Ngo <tienngo@ankvn.com>
2024-04-15 17:08:36 +0700

    first commit

---
+++ Shared/Header.razor.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace trulioo_autofill.Shared;
+
+public partial class Header : ComponentBase
+{
+}
---
+++ Shared/Header.razor.js
@@ -0,0 +1,5 @@
+﻿export class Header {
+  
+}
+
+window.Header = Header;
